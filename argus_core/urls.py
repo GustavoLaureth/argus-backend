e31f94042819f8d19c48633efa553629a5a62e18
Gustavo <gustavolaureth22@gmail.com>
2026-03-12 22:04:18 -0300

    add layouts

--- argus_core/urls.py
+++ argus_core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("", views.index)
+  path("dashboard/", views.dashboard, name="dashboard"),
+  path("create/", views.create, name="create"),
+  path("history/", views.history, name="history"),
+  path("subscription/", views.subscription, name="subscription"),
+  path("profile/", views.profile, name="profile"),
 ]
--- argus_core/views.py
+++ argus_core/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
-def index(request):
-  return render(request, "pages/index.html")
+def dashboard(request):
+  return render(request, "pages/dashboard.html")
+
+def create(request):
+  return render(request, "pages/create.html")
+
+def history(request):
+  return render(request, "pages/history.html")
+
+def subscription(request):
+  return render(request, "pages/subscription.html")
+
+def profile(request):
+  return render(request, "pages/profile.html")
