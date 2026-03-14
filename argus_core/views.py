fc7dc484554a2e68919ddc56d4d4f8936500a0f7
Gustavo <gustavolaureth22@gmail.com>
2026-03-13 22:08:37 -0300

    add login

--- argus_core/views.py
+++ argus_core/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
   return render(request, "pages/dashboard.html")
 
+@login_required
 def create(request):
   return render(request, "pages/create.html")
 
+@login_required
 def history(request):
   return render(request, "pages/history.html")
 
+@login_required
 def subscription(request):
   return render(request, "pages/subscription.html")
 
+@login_required
 def profile(request):
   return render(request, "pages/profile.html")
